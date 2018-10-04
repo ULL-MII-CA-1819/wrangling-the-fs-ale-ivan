2412788589b2289df47783f64b22205309b5769c
marreA <alemarrerodiaz@gmail.com>
2018-10-04 18:33:18 +0100

    Añadiendo los ficheros gulpfile.js y resumen.adoc

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+
+let gulp = require('gulp');
+let shell = require('gulp-shell');
+const mocha = require('gulp-mocha');
+
+
+gulp.task("default", ["test"]); 
+gulp.task("test", () => 
+        gulp.src('./test/test.js', {read: false})
+        .pipe(mocha({reporter: 'nyan'}))
+);
+// gulp.task("test", shell.task("mocha"));
+
+gulp.task('docs', shell.task("asciidoctor docs/resumen.adoc -o docs/index.html"));
+
+gulp.task("pre-install", shell.task([
+    "sudo gem install asciidoctor"
+    ]));
