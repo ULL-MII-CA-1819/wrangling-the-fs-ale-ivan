0130f48667e2c92469df32ff670d6fa440e6d701
Iván García <alu0100693737@ull.edu.es>
2018-10-10 00:03:13 +0200

    Cambiando travis y tarea default gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ let shell = require('gulp-shell');
 const mocha = require('gulp-mocha');
 
 
-gulp.task("default", ["pre-install", "docs"]); 
+gulp.task("default", gulp.series("pre-install", "docs")); 
 gulp.task("test", () =>
         gulp.src('./test/test.js', {read: false})
         .pipe(mocha({reporter: 'nyan'}))
@@ -16,3 +16,5 @@ gulp.task('docs', shell.task("asciidoctor docs/resumen.adoc -o docs/index.html")
 gulp.task("pre-install", shell.task([
     "sudo gem install asciidoctor"
     ]));
+
+gulp.task("travis", )
